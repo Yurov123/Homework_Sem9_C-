16083c0b43f0b2279711eb6005873c3a0e5f5c1f
Konstantin <constantin8271@gmail.com>
2022-03-01 20:22:36 +0300

    Task_67

---
+++ Task_67/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать натуральные числа от N до 1, N задано. N = 5
+
+Console.Clear();
+
+int List(int n)
+{
+    if(n == 0) return 0;
+    Console.Write(n  +" , ");
+    return List(n-1);
+}
+
+Console.WriteLine("Enter interval boundary:"); // Введите границу интервала
+int n = int.Parse(Console.ReadLine());
+List(n);
+Console.WriteLine();
